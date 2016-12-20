5f43c3a7065cc30b921b65fb3fcb1b552ff84a68
burawi <burawi.ba@gmail.com>
2016-12-20 19:17:56 +0100

    first commit

---
+++ conf.js
@@ -0,0 +1,15 @@
+var randomstring = require("randomstring");
+
+module.exports = function (G) {
+
+    var exports = {
+        tayrProp: 'T',
+        table: 'user',
+        tableProps: [
+            'name',
+            'password'
+        ]
+    };
+
+    return exports;
+};
